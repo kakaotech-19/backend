6fdfe107759d66391b4243d34b769a8b74a0dde6
kchranger <kchranger@gmail.com>
2024-11-21 17:48:15 +0900

    불필요한 에노테이션 설정 삭제

--- src/test/java/com/heartsave/todaktodak_api/integrate/IntegrateTestConfiguration.java
+++ src/test/java/com/heartsave/todaktodak_api/integrate/IntegrateTestConfiguration.java
@@ -1,6 +1,5 @@
 package com.heartsave.todaktodak_api.integrate;
 
-
 import com.heartsave.todaktodak_api.ai.client.service.AiClientService;
 import com.heartsave.todaktodak_api.auth.repository.OtpCacheRepository;
 import com.heartsave.todaktodak_api.diary.repository.DiaryReactionRepository;
@@ -16,7 +15,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.mail.javamail.JavaMailSender;
 
-@org.springframework.boot.test.context.TestConfiguration(proxyBeanMethods = false)
+@org.springframework.boot.test.context.TestConfiguration
 @Slf4j
 public class IntegrateTestConfiguration implements ApplicationListener<ContextRefreshedEvent> {
 
